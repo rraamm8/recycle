e2a4a32e65c25a99043df3c4552ff79807daba6f
YEJIN1120 <kyj21120@naver.com>
2025-01-20 14:14:03 +0900

    250120

--- Back/Back/src/main/java/com/project/service/FlaskResultService.java
+++ Back/Back/src/main/java/com/project/service/FlaskResultService.java
@@ -1,9 +1,13 @@
 package com.project.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
 import com.project.domain.FlaskResultDTO;
 import com.project.domain.FlaskResultEntity;
+import com.project.domain.ShapeEntity;
 import com.project.persistence.FlaskResultRepository;
 
 @Service
@@ -15,14 +19,30 @@ public class FlaskResultService {
 	}
 
 	public void saveResult(FlaskResultDTO flaskResult) {
-		// DTO -> Entity 변환
-		FlaskResultEntity entity = new FlaskResultEntity(
-			flaskResult.getVersion(), 
-			flaskResult.getImagePath(),
-		    flaskResult.getImageHeight(), 
-		    flaskResult.getImageWidth()
-		);
+		// DTO의 shapes -> Entity의 shapes 변환
+		List<ShapeEntity> shapeEntities = flaskResult.getShapes().stream()
+		        .map(shapeDTO -> ShapeEntity.builder()
+		            .label(shapeDTO.getLabel())
+		            .points(shapeDTO.getPoints()) // JSON 배열 그대로 저장하거나 변환
+		            .shapeType(shapeDTO.getShapeType())
+		            .groupId(shapeDTO.getGroupId())
+		            .flags(shapeDTO.getFlags())
+		            .flaskResult(null) // 양방향 관계 처리
+		            .build())
+		        .collect(Collectors.toList());
 		
+		// DTO -> Entity 변환
+		 FlaskResultEntity entity = FlaskResultEntity.builder()
+			        .version(flaskResult.getVersion())
+			        .imagePath(flaskResult.getImagePath())
+			        .imageHeight(flaskResult.getImageHeight())
+			        .imageWidth(flaskResult.getImageWidth())
+			        .shapes(shapeEntities)
+			        .build();
+		 
+		 // shapes에 FlaskResultEntity 설정 (양방향 관계 설정)
+		 shapeEntities.forEach(shape -> shape.setFlaskResult(entity));
+		 
 		// 데이터베이스 저장
 		repository.save(entity);
 	}
